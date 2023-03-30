1416e81860b0adeaef015b6db9f9af10afa5b7ea
VireshAmbardar <ambardarviresh18@gmail.com>
2023-03-30 23:38:51 +0530

    Plear add addition and multiplication function

---
+++ test.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+## run this command first ./Test/Scripts/activate
+
+a = 50
+b = 10 
+
+##chaitu make a addition file here
+def add():
+    # code here
+
+    return 
+
+## Sheaker make a multiplication file below
+def miltiplication():
+    # code here
+    
+    return
